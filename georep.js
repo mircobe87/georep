763b4a1d22cd8b63aa6997d639c618e1ede78227
Maurizio <pratesi.maurizio@gmail.com>
2013-11-15 16:21:01 +0100

    implementata e testata la funzione checkUser

--- georep.js
+++ georep.js
@@ -79,3 +79,54 @@ georep.config.setURLServer = function(URLServer, callback){
 	}
 }
 
+
+/**
+ * Controlla se un utente è registrato sul server CouchDB (geocouch).
+ * 
+ * callback ( function(err, data) ):
+ * 		funzione di callback, NON OPZIONALE, chiamata sia in caso di errore che di successo;
+ *         err: oggetto che descrive l'errore, se si è verificato;
+ *        data: true se l'utente è già registrato, false se non lo è .
+ */
+georep.checkUser = function(callback){
+	/* callback è obbligatorio perchè checkUser() chiama $.ajax() che è asincrona */	
+	if (!callback){
+		throw "checkUser() richiede il paramentro callback OBBLIGATORIO."
+	}
+	/*controllo se l'utente è stato configurato*/
+	else if (!georep.options.user.base64){
+		callback({err:"checkUser() richiede che l'utente sia configurato.", param: georep.options.user},undefined);
+	}
+	/*controllo se il server è stato configurato*/
+	else if (!georep.options.db.host){
+		callback({err:"checkUser() richiede che il server sia configurato.", param: georep.options.db},undefined);
+	}
+	else{
+		/* richiedo info sul db, usando come credenziali di accesso quelle dell'utente georep.options.user, 
+           se l'accesso al db viene negato, significa che l'utente non è registrato */
+		$.ajax({
+			url: georep.options.db.proto+georep.options.db.host+':'+georep.options.db.port,
+			type: 'GET',
+			timeout: 3000,
+			headers: {
+				'Authorization': 'Basic '+georep.options.user.base64
+			},
+			success: function(data){
+				/*console.log("Utente gia' registrato "+ data);*/
+				callback(undefined, {isRegistered: true});
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				/*console.log('jqXHR: '+ jqXHR + '\ntextStatus: '+textStatus + '\nerrorThrown: '+errorThrown);*/
+				if (errorThrown == 'Unauthorized'){
+					/*console.log("Utente NON registrato");*/
+					callback(undefined, {isRegistered: false});
+				}
+				else{
+					callback({err: "Impossibile capire se l'utente e' registrato", jqXHR: jqXHR, textStatus: textStatus, errorThrown: errorThrown}, undefined);
+				}
+			},			
+		});
+	}
+}
+ 
+
